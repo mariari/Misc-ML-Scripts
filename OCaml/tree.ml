c7fa4cfc6018c7fa52725b0e36bf59920d6108ae
mariari <nymphet@protonmail.com>
2018-08-21 20:48:39 -0400

    changed out the Deque for a Queue, and fixed readability a bit

--- OCaml/tree.ml
+++ OCaml/tree.ml
@@ -5,9 +5,9 @@ type 'a tree_mut =
     EmptyM
   | SingleM of 'a
   | BranchM of
-      {ele : 'a;
-       l   : ('a tree_mut) ref;
-       r   : ('a tree_mut) ref;
+      { ele : 'a;
+        l   : ('a tree_mut) ref;
+        r   : ('a tree_mut) ref;
       }
 
 
@@ -26,13 +26,15 @@ exception Empty
 let rec purify_tree = function
     EmptyM    -> Empt
   | SingleM x -> Single x
-  | BranchM {l ; ele ; r} -> Branch {l = (purify_tree !l);
-                                     ele = ele;
-                                     r = (purify_tree !r)}
+  | BranchM x -> Branch { l   = purify_tree !(x.l);
+                          ele = x.ele;
+                          r   = purify_tree !(x.r)
+                        }
 
 module Tree = struct
 
   type 'a t = 'a tree
+
   let ele_exn = function
       BranchM {ele} -> ele
     | SingleM ele   -> ele
@@ -41,26 +43,26 @@ module Tree = struct
   let from_list_bredth = function
       [] -> Empt
     | x :: xs ->
-       let queue = (Deque.create ()) in
+       let queue = Queue.create () in
        let root  = ref (SingleM x) in
-       Deque.enqueue_back queue root;
+       Queue.enqueue queue root;
 
        let rec build = function
            []  -> ()
-         | [x] -> let node = Deque.dequeue_front_exn queue in
+         | [x] -> let node = Queue.dequeue_exn queue in
                   node := BranchM { l   = ref (SingleM x);
                                     ele = ele_exn !node;
-                                    r   = ref EmptyM}
-         | x :: y :: xs -> let node = Deque.dequeue_front_exn queue in
+                                    r   = ref EmptyM
+                                  }
+         | x :: y :: xs -> let node = Queue.dequeue_exn queue in
                            let l = ref (SingleM x) in
                            let r = ref (SingleM y) in
-                           Deque.enqueue_back queue l;
-                           Deque.enqueue_back queue r;
-                           node := BranchM {l = l;
-                                            ele = ele_exn !node;
-                                            r = r};
+                           Queue.enqueue_all queue [l; r];
+                           node := BranchM { l = l;
+                                             ele = ele_exn !node;
+                                             r = r;
+                                           };
                            build xs
-
        in
        build xs;
        purify_tree !root
