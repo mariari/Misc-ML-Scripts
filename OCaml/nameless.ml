cc4c78da4033f15e89eeb1664a18c5034e17eb96
mariari <nymphet@protonmail.com>
2018-09-02 03:50:43 -0400

    added shift,sub, and a normal eval and a call-by-value eval

--- OCaml/nameless.ml
+++ OCaml/nameless.ml
@@ -19,6 +19,10 @@ type context = {
 
 exception Foo of string
 
+let rec eval f l = match f l with
+    Some x -> eval f x
+  | None   -> l
+
 module Name_lambda : sig
   type t = context
   val create        : (string * int) list -> t
@@ -53,7 +57,7 @@ end = struct
                                                   ^ " to the table"))
 
 
-  let rec restore_names  ({depth ; names = {forward ; backward}} as c) = function
+  let rec restore_names ({depth ; names = {forward ; backward}} as c) = function
       App (f,s)      -> App (restore_names c f, restore_names c s)
     | Abs (str,term) -> let new_depth = depth + 1 in
                         Abs (str, restore_names
@@ -62,7 +66,7 @@ end = struct
                                               backward = Map.update backward
                                                                     (-new_depth)
                                                                     (fun _ -> str)}}
-                               term)
+                                    term)
     | Var num -> match Map.find backward (num - depth) with
                   Some str -> Var str
                 | None     -> raise (Foo String.("Variable not in map, try adding"
@@ -70,13 +74,76 @@ end = struct
                                                  ^ " to the table"))
 end
 
-module Nl = Name_lambda
+module NL = Name_lambda
 
-let given = Nl.create [("x",4); ("y", 3); ("z", 2); ("b", 1); ("a", 0)]
+module Eval = struct
 
-let test_lam = Abs ("x", App (Var "x", App (Var "y", Var "z")))
+  type t = int lambda
 
-let is_id context lam = let numLam = Nl.remove_names context lam in
-                        Nl.restore_names context numLam = lam
+  let shift d l =
+    let rec walk count = function
+        App (f, s)    -> App (walk count f, walk count s)
+      | Abs (s, term) -> Abs (s, walk (count + 1) term)
+      | Var num       -> if num >= count
+                         then Var (num + d)
+                         else Var num
+    in walk 0 l
+
+  let subs var new_term l =
+    let rec walk count = function
+        App (f, s)    -> App (walk count f, walk count s)
+      | Abs (s, term) -> Abs (s, walk (count + 1) term)
+      | Var num       -> if num = var + count
+                         then shift count new_term
+                         else Var num
+    in walk 0 l
+
+  let is_val = function
+      Abs _ -> true
+    | _     -> false
+
+  let is_vals = List.for_all ~f:is_val
+
+
+
+  (* Evaluates the abstraction on the left argument and places t2 inside of it *)
+  let shift_l v1 t2 = let Abs(_,t1) = shift (-1) v1 [@@warning "-8"] in
+                      Some (subs 0 t2 t1)
+
+  let rec eval1_normal = function
+      App (v1, t2) when is_val v1  -> shift_l v1 t2
+    | Abs (str, (App (_,_) as t2)) -> Option.map (eval1_normal t2) (fun x -> Abs (str, x))
+    | _                            -> None
+
+  let eval_normal = eval eval1_normal
+
+  let rec eval1_value = function
+      App (v1, v2) when is_vals [v2;v1] -> shift_l v1 v2
+    | App (v1, t2) when is_val v1 -> Option.map (eval1_value t2) (fun t2' -> App (v1, t2'))
+    | App (t1, t2)                -> Option.map (eval1_value t1) (fun t1' -> App (t1', t2))
+    | _                           -> None
+
+  let eval_value = eval eval1_value
+end
+
+
+
+
+(*Testing code _______________________________________________*)
+
+let given = NL.create [("x", 4); ("y", 3); ("z", 2); ("b", 1); ("a", 0)]
+
+let test_lam = Abs ("x", App (App (Var "y", Var "z"), Var "x"))
+
+let is_id context lam = let numLam = NL.remove_names context lam in
+                        NL.restore_names context numLam = lam
 
 (*  is_id given test_lam;; => true! *)
+
+let test_app = App (test_lam, Var "y")
+
+let identity = Abs ("x", Var "x")
+
+let test_app_2 = App (identity, App (identity, Abs ("z", App (identity, Var "z"))))
+
+(* (Eval.eval_value (NL.remove_names given test_app_2));; *)
