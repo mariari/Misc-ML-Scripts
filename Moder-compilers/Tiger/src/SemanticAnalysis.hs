5ac15117ab820bae0371faf250645a7855eeea75
mariari <nymphet@protonmail.com>
2018-09-11 12:40:53 -0400

    designed the monad state type of the translation

--- Moder-compilers/Tiger/src/SemanticAnalysis.hs
+++ Moder-compilers/Tiger/src/SemanticAnalysis.hs
@@ -1,33 +1,60 @@
-module TypeChecker where
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 
-import ProgramTypes
+module SemanticAnalysis where
+
+import qualified ProgramTypes as PT
+import qualified TigerType    as Absyn
 
 import           Data.Unique
 import qualified Data.Symbol as S
 import qualified Data.Map.Strict as Map -- we are use ordering in symbols, so we can't use HashMap
+import           Control.Monad.State.Lazy
 
 -- Environment
-type TypeMap = SymMap Type     -- for types
-type EnvMap  = SymMap EnvEntry -- for functions and variables
+type TypeMap = PT.SymMap PT.Type  -- for types
+type EnvMap  = PT.SymMap EnvEntry -- for functions and variables
 
-data EnvEntry = VarEntry {ty :: Type}
-              | FunEntry {formals :: [Type]
-                         ,result  :: Type}
+data Translation = Trans { tm :: TypeMap
+                         , em :: EnvMap
+                         } deriving Show
+
+type MonadTranslation m    = MonadState Translation m
+type MonadTranlsationImp m = (MonadIO m, MonadTranslation m)
+
+data EnvEntry = VarEntry {ty :: PT.Type}
+              | FunEntry {formals :: [PT.Type]
+                         ,result  :: PT.Type}
               deriving Show
 
 baseTenv :: TypeMap
-baseTenv = Map.fromList [(S.intern "int",    INT)
-                        ,(S.intern "string", STRING)
-                        ,(S.intern "nil",    NIL)]
+baseTenv = Map.fromList [(S.intern "int",    PT.INT)
+                        ,(S.intern "string", PT.STRING)
+                        ,(S.intern "nil",    PT.NIL)]
 
 baseVenv :: EnvMap
-baseVenv = Map.fromList [(S.intern "print",     FunEntry [STRING] UNIT)
-                        ,(S.intern "flush",     FunEntry []       UNIT)
-                        ,(S.intern "getchar",   FunEntry []       STRING)
-                        ,(S.intern "ord",       FunEntry [STRING] INT)
-                        ,(S.intern "chr",       FunEntry [INT]    STRING)
-                        ,(S.intern "size",      FunEntry [STRING] INT)
-                        ,(S.intern "not",       FunEntry [INT]    INT)
-                        ,(S.intern "exit",      FunEntry [INT]    UNIT)
-                        ,(S.intern "substring", FunEntry [STRING, INT, INT] STRING)
-                        ,(S.intern "concat",    FunEntry [STRING, STRING]   STRING)]
+baseVenv = Map.fromList [(S.intern "print",     FunEntry [PT.STRING] PT.UNIT)
+                        ,(S.intern "flush",     FunEntry []          PT.UNIT)
+                        ,(S.intern "getchar",   FunEntry []          PT.STRING)
+                        ,(S.intern "ord",       FunEntry [PT.STRING] PT.INT)
+                        ,(S.intern "chr",       FunEntry [PT.INT]    PT.STRING)
+                        ,(S.intern "size",      FunEntry [PT.STRING] PT.INT)
+                        ,(S.intern "not",       FunEntry [PT.INT]    PT.INT)
+                        ,(S.intern "exit",      FunEntry [PT.INT]    PT.UNIT)
+                        ,(S.intern "substring", FunEntry [PT.STRING, PT.INT, PT.INT] PT.STRING)
+                        ,(S.intern "concat",    FunEntry [PT.STRING, PT.STRING]      PT.STRING)]
+
+
+-- Translation type
+-- will be expanded upon in chapter 7
+type TranslateExp = ()
+
+data Expty = Expty { exp :: TranslateExp
+                   , typ :: PT.Type
+                   } deriving Show
+
+transVar :: MonadTranslation m => Absyn.Var -> m TranslateExp
+transVar = undefined
+
+transExp :: MonadTranslation m => Absyn.Exp -> m TranslateExp
+transExp = undefined
